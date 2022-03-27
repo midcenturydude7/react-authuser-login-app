45c729b6ea5959d3cec66fd447669dd809a0456c
midCenturyDude7 <mgriffes@gmail.com>
2022-03-27 08:19:04 -0700

    Basic login functionality established with Express and Node.js

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,18 +1,43 @@
 import React from "react";
+import PropTypes from "prop-types";
+
 import "./Login.css"
 
-export default function Login() {
+async function LoginUser(credentials) {
+  return fetch("http://localhost:8080/login", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify(credentials)
+  })
+  .then(data => data.json())
+}
+
+export default function Login({ setToken }) {
+  const [username, setUsername] = React.useState();
+  const [password, setPassword] = React.useState();
+
+  const handleSubmit = async event => {
+    event.preventDefault();
+    const token = await LoginUser({
+      username,
+      password
+    });
+    setToken(token)
+  }
+
   return (
     <div className="login-wrapper">
       <h1>Please Log In Motherfucker!</h1>
-      <form>
+      <form onSubmit={handleSubmit}>
         <label>
           <p>Username</p>
-          <input type="text" />
+          <input type="text" onChange={event => setUsername(event.target.value)} />
         </label>
         <label>
           <p>Password</p>
-          <input type="password" />
+          <input type="password" onChange={event => setPassword(event.target.value)} />
         </label>
         <div>
           <button type="submit">Submit</button>
@@ -20,4 +45,8 @@ export default function Login() {
       </form>
     </div>
   )
+}
+
+Login.propTypes = {
+  setToken: PropTypes.func.isRequired
 }
